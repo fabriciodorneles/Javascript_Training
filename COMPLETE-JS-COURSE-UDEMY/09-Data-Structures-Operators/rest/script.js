7064522cb6a5d22ed32438e43b1578c1499f16e2
Fabricio Dorneles <fabriciodorneles@gmail.com>
2020-11-09 07:38:34 -0300

    challenge 1

---
+++ COMPLETE-JS-COURSE-UDEMY/09-Data-Structures-Operators/rest/script.js
@@ -0,0 +1,115 @@
+'use strict';
+
+
+
+const openingHours = {
+  thu: {
+    open: 12,
+    close: 22,
+  },
+  fri: {
+    open: 11,
+    close: 23,
+  },
+  sat: {
+    open: 0, // Open 24 hours
+    close: 24,
+  },
+};
+
+// Objects
+const { sat, ...weekdays } = openingHours;
+console.log(weekdays);
+
+const restaurant = {
+  name: 'Classico Italiano',
+  location: 'Via Angelo Tavanti 23, Firenze, Italy',
+  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+
+}
+///////////////////////////////////////
+// Rest Pattern and Parameters
+// 1) Destructuring
+
+// SPREAD, because on RIGHT side of =
+const arr = [1, 2, ...[3, 4]];
+
+// REST, because on LEFT side of =
+const [a, b, ...others] = [1, 2, 3, 4, 5];
+console.log(a, b, others);
+
+const [pizza, , risotto, ...otherFood] = [
+  ...restaurant.mainMenu,
+  ...restaurant.starterMenu,
+];
+console.log(pizza, risotto, otherFood);
+
+
+// 2) Functions
+const add = function (...numbers) {
+  let sum = 0;
+  numbers.map(item => sum+=item)
+  console.log(sum);
+};
+add(2, 3);
+add(5, 3, 7, 2);
+add(8, 2, 5, 3, 2, 1, 4);
+const x = [23, 5, 7];
+add(...x);
+
+// restaurant.orderPizza('mushrooms', 'onion', 'olives', 'spinach');
+// restaurant.orderPizza('mushrooms');
+
+
+// ///////////////////////////////////////
+// // The Spread Operator (...)
+
+// const arr = [7, 8, 9];
+// const badNewArr = [1, 2, arr[0], arr[1], arr[2]];
+// console.log(badNewArr);
+
+// const newArr = [1, 2, ...arr];
+// console.log(newArr);
+
+// console.log(...newArr);
+// console.log(1, 2, 7, 8, 9);
+
+// const newMenu = [...restaurant.mainMenu, 'Gnocci'];
+// console.log(newMenu);
+
+// // Copy array
+// const mainMenuCopy = [...restaurant.mainMenu];
+
+// // Join 2 arrays
+// const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
+// console.log(menu);
+
+// // Iterables: arrays, strings, maps, sets. NOT objects
+// const str = 'Jonas';
+// const letters = [...str, ' ', 'S.'];
+// console.log(letters);
+// console.log(...str);
+// // console.log(`${...str} Schmedtmann`);
+
+// // Real-world example
+// const ingredients = [
+//   // prompt("Let's make pasta! Ingredient 1?"),
+//   // prompt('Ingredient 2?'),
+//   // prompt('Ingredient 3'),
+// ];
+// console.log(ingredients);
+
+// restaurant.orderPasta(ingredients[0], ingredients[1], ingredients[2]);
+// restaurant.orderPasta(...ingredients);
+
+// // Objects
+// const newRestaurant = { foundedIn: 1998, ...restaurant, founder: 'Guiseppe' };
+// console.log(newRestaurant);
+
+// const restaurantCopy = { ...restaurant };
+// restaurantCopy.name = 'Ristorante Roma';
+// console.log(restaurantCopy.name);
+// console.log(restaurant.name);
+
--- COMPLETE-JS-COURSE-UDEMY/09-Data-Structures-Operators/starter/script.js
+++ COMPLETE-JS-COURSE-UDEMY/09-Data-Structures-Operators/starter/script.js
@@ -1,52 +1,58 @@
 'use strict';
 
-const restaurant = {
-  name: 'Classico Italiano',
-  location: 'Via Angelo Tavanti 23, Firenze, Italy',
-  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
-  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
-  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
-  order: function (starterIndex, mainIndex) {
-      return [this.starterMenu[starterIndex],this.mainMenu[mainIndex]];
-  },
-  openingHours: {
-    thu: {
-      open: 12,
-      close: 22,
-    },
-    fri: {
-      open: 11,
-      close: 23,
-    },
-    sat: {
-      open: 0, // Open 24 hours
-      close: 24,
-    },
-  },
-};
+// const restaurant = {
+//   name: 'Classico Italiano',
+//   location: 'Via Angelo Tavanti 23, Firenze, Italy',
+//   categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+//   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+//   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+//   order: function (starterIndex, mainIndex) {
+//       return [this.starterMenu[starterIndex],this.mainMenu[mainIndex]];
+//   },
+//   openingHours: {
+//     thu: {
+//       open: 12,
+//       close: 22,
+//     },
+//     fri: {
+//       open: 11,
+//       close: 23,
+//     },
+//     sat: {
+//       open: 0, // Open 24 hours
+//       close: 24,
+//     },
+//   },
+// };
 
-///////////////////////////
-////  DESTRUCTURING ARRAYS
-///////////////////////////
-let [main ,, secondary] = restaurant.categories;
-console.log(main,secondary);
+// ///////////////////////////
+// ////  DESTRUCTURING ARRAYS
+// ///////////////////////////
+// let [main ,, secondary] = restaurant.categories;
+// console.log(main,secondary);
 
-//TRICK = CHANGING VALUES
-[main, secondary] = [secondary, main];
-console.log(main,secondary);
+// //TRICK = CHANGING VALUES
+// [main, secondary] = [secondary, main];
+// console.log(main,secondary);
 
-// DESTRUCTURING ARRAY IN OBJECT
-const [starter, mainPlate] = restaurant.order(2,0);
-console.log(starter, mainPlate);
+// // DESTRUCTURING ARRAY IN OBJECT
+// const [starter, mainPlate] = restaurant.order(2,0);
+// console.log(starter, mainPlate);
 
-// DESTRUCTURING NESTED ARRAY
-const nested = [2, 4, [5, 6]];
-const [i,,[j,k]] = nested;
-console.log(i,j,k);
+// // DESTRUCTURING NESTED ARRAY
+// const nested = [2, 4, [5, 6]];
+// const [i,,[j,k]] = nested;
+// console.log(i,j,k);
 
-//DEFAULT VALUES WITH DESTRUCTURE
-const [p=1, q=1, r=1] = [8,9];
-console.log(p,q,r);
+// //DEFAULT VALUES WITH DESTRUCTURE
+// const [p=1, q=1, r=1] = [8,9];
+// console.log(p,q,r);
+
+
+console.log(0 && 'Hello' && 1 && 'jonas');
+console.log('Hello' && 0 && 1 && 'jonas');
+console.log('Hello' && 1 && 0 && 'jonas');
+console.log('Hello' && 1 && 1 && 'jonas');
 
 ///////////////////////////////////////
 ////  OBJECTS DESTRUCTURING
